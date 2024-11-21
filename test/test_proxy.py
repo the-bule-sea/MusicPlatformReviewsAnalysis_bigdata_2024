9c61fb09cc2b89365ebc59894dd20950ed43260f
the-bule-sea <2653935465@qq.com>
2024-11-21 20:30:04 +0800

    test: 添加代理IP测试脚本
    - 新增 test_proxy.py 文件，用于测试代理IP的功能
    - 实现了加载代理IP列表、发送请求并打印状态码的功能- 该脚本有助于验证和维护代理IP的有效性

---
+++ test/test_proxy.py
@@ -0,0 +1,22 @@
+import time
+
+import requests
+
+def load_proxies(proxy_file_path):
+    with open(proxy_file_path, 'r') as f:
+        proxies = [line.strip() for line in f.readline()]
+    return proxies
+
+def main():
+    proxy_file_path = 'proxy.txt'
+    proxies = load_proxies(proxy_file_path)
+    url = "https://mail.163.com/"
+    for proxy in proxies:
+        print("当前代理IP"+proxy)
+        r = requests.get(url ,proxies={"http": proxy, "https": proxy})
+        print(r.status_code)
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
+
